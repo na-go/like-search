5b3b6a8de7ef7bf760222770306c52a74e0a84f2
na-go <mitsume.anko@gmail.com>
2023-07-30 23:44:40 +0900

    add gitignore dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const plus = (a, b) => a + b
-
-const main = () => {
-  console.log(plus(1, 2))
-}
-main()
-// # sourceMappingURL=index.js.map
--- dist/index.mjs
+++
@@ -1,7 +0,0 @@
-const plus = (a, b) => a + b
-
-const main = () => {
-  console.log(plus(1, 2))
-}
-main()
-//# sourceMappingURL=index.mjs.map
